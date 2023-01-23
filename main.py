0ee3cf75a444f93898499d212dd90b9ba774e00b
aysebat <aaysebatt@gmail.com>
2023-01-23 08:57:15 +0300

    Working to get the article from arxiv

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-from flask import Flask
+import arxiv
+
+
+class NewsFeed:
+    """Get the News"""
+
+    def __int__(self, data):
+        self.data = data
+
+    def get(self):
+        pass
+
+
+class PaperFeed:
+
+    def __init__(self, query, length):
+        self.query = query
+        self.length = length
+
+    def get_paper(self):
+        search = arxiv.Search(query=self.query,
+                              max_results= self.length,
+                              sort_by=arxiv.SortCriterion.SubmittedDate)
+
+        for result in search.results():
+            print(result.title)
+            print(result.links)
+
+
+
+
+paper = PaperFeed(query="neutrino", length=10)
+print(paper.get_paper())
