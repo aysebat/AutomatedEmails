3773493bf414c1574e269d2b9a1006d6345c379c
aysebat <aaysebatt@gmail.com>
2023-01-25 19:08:28 +0300

    refactoring teh send email from the fro loop

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ import yagmail
 import pandas as pd
 import time
 
+
+def send_email():
+    email = yagmail.SMTP(user='',
+                         password='')
+    paper_feed = PaperFeed(query=row['interest'], length=10).get_paper()
+    email.send(to=row['email'],
+               subject=f"Your {row['interest']} paper for today",
+               contents=f"Hi {row['name']}, \n \n "
+                        f"See what's on about  {row['interest']} paper today. \n \n "
+                        f"{paper_feed} \n \n "
+                        f"Happy Reading \n "
+                        f"Ayse,"
+               )
+
+
 #datetime.datetime.now().hour == 8 and datetime.datetime.now().minute == 18:
 while True:
     executing_time = datetime.time(8, 36, 00)
@@ -13,16 +28,5 @@ while True:
         # read the excel file
         df = pd.read_excel('people.xlsx')
         for index, row in df.iterrows():
-            email = yagmail.SMTP(user='',
-                                 password='')
-
-            paper_feed = PaperFeed(query=row['interest'], length=10).get_paper()
-            email.send(to=row['email'],
-                       subject=f"Your {row['interest']} paper for today",
-                       contents=f"Hi {row['name']}, \n \n "
-                                f"See what's on about  {row['interest']} paper today. \n \n "
-                                f"{paper_feed} \n \n "
-                                f"Happy Reading \n "
-                                f"Ayse,"
-                       )
+            send_email()
     time.sleep(60)
