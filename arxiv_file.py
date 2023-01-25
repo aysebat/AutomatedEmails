3ae0dfbc0839ae84d2923c07ac56b00ab3e51047
aysebat <aaysebatt@gmail.com>
2023-01-25 08:19:16 +0300

    fix the main class

---
+++ arxiv_file.py
@@ -0,0 +1,22 @@
+import arxiv
+
+class PaperFeed:
+
+    def __init__(self, query, length):
+        self.query = query
+        self.length = length
+
+    def get_paper(self):
+        search = arxiv.Search(query=self.query,
+                              max_results= self.length,
+                              sort_by=arxiv.SortCriterion.SubmittedDate)
+        paper_body = ''
+        for result in search.results():
+            paper_body = paper_body +  f"Title: {result.title}" + "\n" + f"Published Date: {result.published}" + "\n" + f"URL: {result.links[0]} \n \n"
+
+
+        return paper_body
+
+if __name__=="__main__":
+    paper = PaperFeed(query="neutrino", length=10)
+    print(paper.get_paper())
