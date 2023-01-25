63a37dbc6d6b90d940ea2e3f828b070416a68a90
aysebat <aaysebatt@gmail.com>
2023-01-25 08:33:55 +0300

    run the script with proper time

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
+import datetime
+
 from arxiv_file import PaperFeed
 import yagmail
 import pandas as pd
+import time
 
-# read the excel file
-df = pd.read_excel('people.xlsx')
+#datetime.datetime.now().hour == 8 and datetime.datetime.now().minute == 18:
+while True:
+    executing_time = datetime.time(8, 30, 00)
+    if executing_time.hour:
+        print("Executing...")
+        # read the excel file
+        df = pd.read_excel('people.xlsx')
+        for index, row in df.iterrows():
+            email = yagmail.SMTP(user='',
+                                 password='')
 
-for index, row in df.iterrows():
-    email = yagmail.SMTP(user='',
-                         password='')
-
-    paper_feed = PaperFeed(query=row['interest'], length=10).get_paper()
-    email.send(to=row['email'],
-               subject=f"Your {row['interest']} paper for today",
-               contents=f"Hi {row['name']}, \n \n "
-                        f"See what's on about  {row['interest']} paper today. \n \n "
-                        f"{paper_feed} \n \n "
-                        f"Happy Reading \n "
-                        f"Ayse,"
-               )
+            paper_feed = PaperFeed(query=row['interest'], length=10).get_paper()
+            email.send(to=row['email'],
+                       subject=f"Your {row['interest']} paper for today",
+                       contents=f"Hi {row['name']}, \n \n "
+                                f"See what's on about  {row['interest']} paper today. \n \n "
+                                f"{paper_feed} \n \n "
+                                f"Happy Reading \n "
+                                f"Ayse,"
+                       )
+    time.sleep(60)
